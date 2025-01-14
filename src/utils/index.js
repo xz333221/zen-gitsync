7611d76b2cfc8e8f25c3e9abaebabeed2c49a160
徐泽 <569552263@qq.com>
2025-01-14 13:49:17 +0800

    提交

--- src/utils/index.js
+++ src/utils/index.js
@@ -76,6 +76,7 @@ const coloredLog = (...args) => {
 
   const start_line = '┌' + '─'.repeat(terminalWidth - 2) + '┐';
   const end_line = '└' + '─'.repeat(terminalWidth - 2) + '┘';
+  console.log(`args ==> `, args)
   let _args = args.filter(arg => !!arg).map(arg => arg.split('\n')).flat().filter(arg => arg.trim() !== '');
   console.log(start_line);
   _args.map(async (arg, i) => {
