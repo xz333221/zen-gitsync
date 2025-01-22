79e86e9b627b700d1a7e4480e291688f7c79e732
徐泽 <569552263@qq.com>
2025-01-22 22:54:52 +0800

    submit

--- src/gitCommit.js
+++ src/gitCommit.js
@@ -220,9 +220,7 @@ class GitCommit {
         this.judgeRemote()
 
         this.exec_push()
-
       }else{
-        console.log(`this.statusOutput ==> `, this.statusOutput)
         if (this.statusOutput.includes('use "git push')) {
           this.exec_push()
         } else if (this.statusOutput.includes('use "git pull')) {
