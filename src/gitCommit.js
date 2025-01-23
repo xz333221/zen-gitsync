035f45626c4c5bc7acba2e3ce60fd1453d95acf8
徐泽 <569552263@qq.com>
2025-01-23 10:49:49 +0800

    submit

--- src/gitCommit.js
+++ src/gitCommit.js
@@ -184,6 +184,7 @@ class GitCommit {
           // // 如果 --ff-only 拉取失败，尝试普通的 git pull
           // console.log(chalk.yellow('⚠️ 无法快进合并，尝试普通合并...'));
           // await this.execPull()
+          new Error(pullError)
         }
       } else {
         spinner.stop();
