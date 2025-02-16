79dc8145e9861ee035b7b39faa2697d97df9522a
徐泽 <569552263@qq.com>
2025-02-16 15:18:39 +0800

    提交

--- src/utils/index.js
+++ src/utils/index.js
@@ -426,7 +426,7 @@ function delay(timeout) {
 }
 async function judgeRemote() {
   try {
-    await delay(100)
+    await delay(1000)
     const spinner = ora('正在检查远程更新...').start();
     // 检查是否有远程更新
     // 先获取远程最新状态
