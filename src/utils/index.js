887f8fd1da1437f36495ea1bbf55362a7e683096
徐泽 <569552263@qq.com>
2025-02-16 13:28:41 +0800

    提交

--- src/utils/index.js
+++ src/utils/index.js
@@ -164,6 +164,7 @@ const coloredLog = (...args) => {
   const commandLine = args[0];
   const content = args[1];
   const type = args[2] || 'common';
+  console.log(`commandLine, content, type ==> `, commandLine, content, type)
   tableLog(commandLine, content, type);
 }
 const errorLog = (commandLine, content) => {
