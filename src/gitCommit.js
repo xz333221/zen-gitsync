9c71bc393858cc230c5cf4bfe3d9644c1e39d99d
徐泽 <569552263@qq.com>
2025-02-19 17:54:33 +0800

    提交

--- src/gitCommit.js
+++ src/gitCommit.js
@@ -29,8 +29,12 @@ function startCountdown(interval) {
 
     const message = [
       chalk.green.bold('🕒 倒计时'),
+      `🕒 ${chalk.green.bold('🕒 倒计时')}`
+      `工作目录: ${chalk.cyan(getCwd())}`,
       chalk.cyan(`下次提交: ${formattedTime}`),
+      `下次提交: ${chalk.cyan(formattedTime)}`,
       chalk.yellow(`剩余时间: ${duration}`),
+      `剩余时间: ${chalk.yellow(duration)}`,
       chalk.dim('按 Ctrl+C 终止进程')
     ].join('\n');
 
